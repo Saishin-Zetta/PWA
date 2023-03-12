ff334703b6ca005d1b15f6ac748e4d3f3ff897cb
Andrew <andrewy3295@gmail.com>
2023-03-11 22:40:51 -0600

    still figuring it out

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,10 +19,10 @@ export const putDb = async (content) => {
   console.log('Post to the database');
 
   // Create a connection to the database database and version we want to use.
-  const todosDb = await openDB('jate', 1);
+  const jate_db = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = todosDb.transaction('jate', 'readwrite');
+  const tx = jate_db.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
@@ -42,21 +42,22 @@ export const getDb = async () => {
   console.log('GET all from the database');
 
   // Create a connection to the database database and version we want to use.
-  const todosDb = await openDB('jate', 1);
+  const jate_db = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = todosDb.transaction('jate', 'readonly');
+  const tx = jate_db.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  // using store.get(1) because it's referring to the jate database version 1
+  const request = store.get(1);
 
    // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result?.value;
 };
 
 initdb();
